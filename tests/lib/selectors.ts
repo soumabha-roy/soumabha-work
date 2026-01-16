e39b57f418425316718f0172e8dbcfd6100758e0
soumabha <soumabha@itobuz.com>
2026-01-16 17:25:48 +0530

    dob wip

--- tests/lib/selectors.ts
+++ tests/lib/selectors.ts
@@ -2,4 +2,7 @@ export const selectors = {
   logo: '[class="logo"]',
   loginButton: '.login_sec',
   phoneField: '.phone input',
+  login: '.rs-modal-body > button.my-4.rs-btn.rs-btn-primary.rs-btn-block',
+  opt: '[autocomplete="one-time-code"]',
+  verify: 'div > div.text-center.rs-modal-body > button',
 };
--- tests/registration-flow.spec.ts
+++ tests/registration-flow.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { selectors } from './lib/selectors';
+import { faker } from '@faker-js/faker';
 
 test.describe('Registration flow', () => {
   test.beforeEach(async ({ page }) => {
@@ -16,20 +17,41 @@ test.describe('Registration flow', () => {
     await loginButton.click();
     const phoneNumberField = await page.locator(selectors.phoneField);
     await expect(phoneNumberField).toBeVisible();
-    await page.locator(selectors.phoneField).fill('1234567890');
-    const loginButtonEl = page.locator('.rs-modal-body > button.my-4.rs-btn.rs-btn-primary.rs-btn-block');
+    await page.locator(selectors.phoneField).fill(faker.string.numeric(10));
+    const loginButtonEl = page.locator(selectors.login);
     expect(loginButtonEl).toBeEnabled();
     await loginButtonEl.click();
     //Type opt
-    const otpFields = page.locator('[autocomplete="one-time-code"]');
+    const otpFields = page.locator(selectors.opt);
     await expect(otpFields.first()).toBeVisible();
     
     const otpDigits = ['1', '2', '3', '4'];
     for (let i = 0; i < 4; i++) {
       await otpFields.nth(i).fill(otpDigits[i]);
     }
-    const verifyButton = page.locator('div > div.text-center.rs-modal-body > button');
+    const verifyButton = page.locator('.text-center.rs-modal-body button');
     expect(verifyButton).toBeEnabled();
     await verifyButton.click();
+    await page.waitForTimeout(2000);
+
+    //Update profile
+    //Select DOB
+    const dobField = page.locator('div:nth-child(3) > span');
+    expect(dobField).toBeVisible();
+    await dobField.click();
+    const calender = page.locator('.w-100 .editField')
+    await calender.click();
+    // const confirm = page.locator('div.text-center.confirm > button');
+    // await confirm.click();
+    // await page.waitForTimeout(1000);
+    // Select from dropdown option
+    // const dropdownButton = page.locator('.editField');
+    // await dropdownButton.last().click()
+    // const dropdown = page.locator('.custom-select-dropdown');
+    // await expect(dropdown).toBeVisible();
+    // await dropdown.click();
+    // const dropdownOption = page.locator('.custom-select-dropdown li');
+    // await dropdownOption.first().click();
+    
   });
 });
